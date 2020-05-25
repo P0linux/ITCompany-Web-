fe7ce996f8ec56342cf9c0576544325a006f1aa7
P0linux <pollykr2000@gmail.com>
2020-05-25 18:49:37 +0300

    Change something

--- WebApiApp/Controllers/DepartmentController.cs
+++ WebApiApp/Controllers/DepartmentController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WebApiApp.Business;
+using WebApiApp.Business.Interfaces;
 using WebApiApp.Models;
 
 namespace WebApiApp.Controllers
