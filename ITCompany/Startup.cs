d9008290e22922db2974bf51bb8d766168c06abf
P0linux <pollykr2000@gmail.com>
2020-05-26 16:50:33 +0300

    Add Identity service

--- ITCompany/Startup.cs
+++ ITCompany/Startup.cs
@@ -7,10 +7,12 @@ using ITCompany.Business;
 using ITCompany.Business.Interfaces;
 using ITCompany.Business.Services;
 using ITCompany.Data;
+using ITCompany.Data.Entities;
 using ITCompany.Data.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -46,6 +48,16 @@ namespace ITCompany
             services.AddTransient<IEmployeeService, EmployeeService>();
             services.AddTransient<IDepartmentService, DepartmentService>();
             services.AddTransient<ICreatorService, CreatorService>();
+
+            services.AddIdentity<User, IdentityRole>(opt =>
+            {
+                opt.Password.RequireUppercase = false;
+                opt.Password.RequireDigit = false;
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequiredLength = 4;
+                opt.User.RequireUniqueEmail = true;
+            })
+            .AddEntityFrameworkStores<ApplicationContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
